ca39f02f1558d2a01f07a544eb41604817676e82
Ayush Kumar Sinha <ayushkr1322@gmail.com>
2022-12-29 17:45:26 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,85 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      'Im baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry'
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 5,
+    name: 'Lance Jarvis',
+    job: 'the boss',
+    image: 'https://testimonialapi.toolcarton.com/avatar/8.jpg',
+    text: 'We have been using product for last one year, and I have to say that it has transformed the way we do business. Thank you for awesome service',
+  },
+
+  {
+    id: 6,
+    name: 'Juliet Wright',
+    job: 'Investor',
+    image: 'https://testimonialapi.toolcarton.com/avatar/2.jpg',
+    text: 'It has changed the way I used the website. Product lets you create anything you envision and it does it so easy and flawless. I cant imagine not working with the service.',
+  },
+
+  {
+    id: 7,
+    name: 'Diane Smith',
+    job: 'Sales Head',
+    image: 'https://testimonialapi.toolcarton.com/avatar/9.jpg',
+    text: 'Team offers a powerful suite of tools that every client must have. And if you get stuck their support team will help out. Its is fast, reliable and has no complications at all',
+  },
+
+    {
+    id: 8,
+    name: 'Michael D',
+    job: 'Investor',
+    image: 'https://testimonialapi.toolcarton.com/avatar/10.jpg',
+    text: 'Insights from service drove 30% lift in our conversions, this product service is essential for your company growth strategy',
+  },
+    {
+    id: 9,
+    name: 'Kylie Lutz',
+    job: 'Freelancer',
+    image: 'https://testimonialapi.toolcarton.com/avatar/4.jpg',
+    text: 'The company really helped with my personal branding - everything from conception to execution was amazing and super professional. Looking forwards for upgradation',
+  },
+    {
+    id: 10,
+    name: 'Mark Twin',
+    job: 'Developer',
+    image: 'https://testimonialapi.toolcarton.com/avatar/5.jpg',
+    text: ' Best company I have ever deal with online! Quick delivery and excellent customer service. Will definetly upgrade the service',
+  },
+];
+
+export default reviews;
